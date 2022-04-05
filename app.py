7125b1f6d628fde1080d1b8f604a19863d140578
Malia Absi <maliabsi@Malias-MacBook-Pro.local>
2022-04-05 17:27:38 -0400

    login route

--- app.py
+++ app.py
@@ -77,4 +77,9 @@ def signup():
     return flask.render_template("sign-up.html")
 
 
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
