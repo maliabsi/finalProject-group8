e200bf5a41909cba4f458efa17a9b770c5f8fdc4
Malia Absi <maliabsi@Malias-MBP.wifi.gsu.edu>
2022-04-07 16:09:28 -0400

    login / signup styled

--- app.py
+++ app.py
@@ -34,6 +34,42 @@ def index():
     return flask.render_template("index.html")
 
 
+import os
+import flask
+
+from dotenv import find_dotenv, load_dotenv
+from flask_login import current_user, LoginManager, login_user, logout_user
+from stytch import Client
+from models import db, Users
+
+
+load_dotenv(find_dotenv())
+app = flask.Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("NEW_DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """loads  current user"""
+    return Users.query.get(int(user_id))
+
+
+@app.route("/")
+def index():
+    """index page: more!"""
+    return flask.render_template("index.html")
+
+
 # This needs to be tested live. Stytch will not send to a  non-https:// URL.
 @app.route("/authenticate")
 def authenticate():
@@ -53,10 +89,10 @@ def authenticate():
 
     # Authenticates
     response = client.oauth.authenticate(token)
-    print("response:", response)
 
-    # If the response is a 200, the user is verified and can be logged in (Copied from Stytch API docs)
-    if response == 200:
+    # If the response is a 200, the user is verified and can be logged in
+    # (Copied from Stytch API docs)
+    if response.status_code == 200:
         if Users.query.filter_by(token=token).first is None:
             flask.redirect(flask.url_for("signup"), token)
 
@@ -70,6 +106,21 @@ def authenticate():
         print("Not authorized")
         flask.flash("Google was unable to authenticate you. Please try again.")
         flask.redirect(flask.url_for("login"))
+
+
+@app.route("/sign-up")
+def signup():
+    return flask.render_template("sign-up.html")
+
+
+@app.route("/login")
+def login():
+    return flask.render_template("login.html")
+
+
+@app.route("/communities")
+def visit_communities():
+    return flask.render_template("communities.html")
 
 
 app.run(host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True)
