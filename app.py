3355a0721708fcc93d03702c795067c99e7ef671
mcfralish <mcfralish@gmail.com>
2022-04-07 07:52:18 -0400

    try again on auth testing

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def authenticate():
     # print("content:", response)
 
     # stytch_id = response._content["user_id"]
-    stytch_id = response._content[5].decode("UTF-8")
+    stytch_id = response._content[5]
     print("userid:", stytch_id)
 
     # If the response is a 200, the user is verified and can be logged in
