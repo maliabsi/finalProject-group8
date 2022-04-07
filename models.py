a538799b0c032fc33b7ce72577606ee503260e86
Ryan Garland <ryangarland211@gmail.com>
2022-04-07 16:35:45 -0400

    this maybe a fix

--- models.py
+++ models.py
@@ -12,7 +12,7 @@ class Users(UserMixin, db.Model, Base):
     """Users table created"""
 
     id = db.Column(db.Integer, primary_key=True)
-    stytchid = db.Column(db.String(60))
+    stytch_id = db.Column(db.String(60))
     email = db.Column(db.String(60))
 
 
@@ -21,10 +21,12 @@ class Communties(db.Model, Base):
 
     id = db.Column(db.Integer, primary_key=True)
     community_name = db.Column(db.String(120), nullable=False)
-    creator_user_id = relationship("Users", foreign_keys="Users.id")
+    creator_user_id = db.Column(db.Integer, db.ForeignKey(Users.id), primary_key=True)
     num_of_collaborators = db.Column(db.Integer, nullable=False)
     num_of_events = db.Column(db.Integer, nullable=False)
     num_of_charties = db.Column(db.Integer, nullable=False)
+
+    user = relationship("Users", foreign_keys="Users.id")
 
 
 class Events(db.Model, Base):
@@ -32,14 +34,17 @@ class Events(db.Model, Base):
 
     id = db.Column(db.Integer, primary_key=True)
     event_name = db.Column(db.String(120), nullable=False)
-    creator_user_id = relationship("Users", foreign_keys="Users.id")
+    creator_user_id = db.Column(db.Integer, db.ForeignKey(Users.id), primary_key=True)
     num_of_collaborators = db.Column(db.Integer, nullable=False)
     event_decription = db.Column(db.String(500), nullable=False)
     event_date = db.Column(db.String(120), nullable=False)
     event_time = db.Column(db.String(120), nullable=False)
     content = db.Column(db.String(500), nullable=False)
     num_of_event_participants = db.Column(db.String(120), nullable=False)
-    community_id = relationship("Communities", foreign_keys="Communities.id")
+    community_id = db.Column(db.Integer, db.ForeignKey(Communties.id), primary_key=True)
+    community = relationship("Communities", foreign_keys="Communities.id")
+
+    user = relationship("Users", foreign_keys="Users.id")
 
 
 class Participants(db.Model, Base):
@@ -47,8 +52,12 @@ class Participants(db.Model, Base):
 
     id = db.Column(db.Integer, primary_key=True)
     event_name = db.Column(db.String(120), nullable=False)
-    participant_user_id = relationship("Users", foreign_keys="Users.id")
-    event_id = relationship("Events", foreign_keys="Events.id")
+    participant_user_id = db.Column(
+        db.Integer, db.ForeignKey(Users.id), primary_key=True
+    )
+    participant = relationship("Users", foreign_keys="Users.id")
+    event_id = db.Column(db.Integer, db.ForeignKey(Events.id), primary_key=True)
+    event = relationship("Events", foreign_keys="Events.id")
 
 
 class Colaborators(db.Model, Base):
@@ -56,5 +65,9 @@ class Colaborators(db.Model, Base):
 
     id = db.Column(db.Integer, primary_key=True)
     event_name = db.Column(db.String(120), nullable=False)
-    collaborator_user_id = relationship("Users", foreign_keys="Users.id")
-    event_id = relationship("Events", foreign_keys="Events.id")
+    collaborator_user_id = db.Column(
+        db.Integer, db.ForeignKey(Users.id), primary_key=True
+    )
+    collaborator = relationship("Users", foreign_keys="Users.id")
+    event_id = db.Column(db.Integer, db.ForeignKey(Events.id), primary_key=True)
+    event = relationship("Events", foreign_keys="Events.id")
